7b6bba2df5e1523f5e9836dd43e6ebd603755df8
Umair113 <Umair113@users.noreply.github.com>
2020-06-19 22:14:11 +0500

    Pushing to surge

--- src/Child.js
+++ src/Child.js
@@ -9,7 +9,7 @@ const Child=(props)=>{
         <div>
         <h3>{props.name}, james {props.name} &nbsp; </h3>
          <h4>And Secret id is {counterValue[0]}  </h4>
-        <h3>Hey there , I am using CounterContext</h3>
+        <h3>Hi there , I am using CounterContext</h3>
          <button onClick={()=>{counterValue[1](++counterValue[0])}}>
              Increment Context
         </button>
